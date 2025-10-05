7317bff21aa5fddeed4dfd4e666f98be3bbe8838
simhadri <vakacharlasimhadri@gmail.com>
2025-10-05 11:00:18 +0530

    some message

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#no space before or after equal
+PERSON1=John
+PERSON2=Raheem
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, Iam fine. How are you ?"
+echo "$PERSON1:: What are you learning recently"
+echo "$PERSON2:: Iam learning shell scriptinh $PERSON1"
