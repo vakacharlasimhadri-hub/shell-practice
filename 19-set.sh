f081b4f1668c4ec1f1229061db611acfce406b7a
simhadri <vakacharlasimhadri@gmail.com>
2025-10-08 23:38:37 +0530

    sesssion-06

--- 19-set.sh
+++ 19-set.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 set -e
+
+error (){
+    echo "there is an error"
+}
+
+trap error ERR
+
 echo "Hello.."
 echo "Beforre error.."
- cknsk;snndj
- echo "After error"
+cknsk;snndj # here shell understands there is an error and signal is ERR
+echo "After error"
